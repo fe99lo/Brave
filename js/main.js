2be08d0e120504c9fa07cf3d73880f0ace95cd38
root <root@localhost>
2020-08-21 12:15:49 +0300

    publishing awesome pages 1598001349278

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Add all your JS here
+<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+<script type="text/babel">
+function student(name, ...john) {
+    console.log(name);
+    console.log(john); 
+}
+student('myname', 10, 20, 30, 40, 50);
+</script>
