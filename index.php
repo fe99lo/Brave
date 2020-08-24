4d3495efe9c16fb539cc6e316a0e4a7cf5a0e623
root <root@localhost>
2020-08-25 00:49:50 +0300

    publishing awesome pages 1598305790955

---
+++ index.php
@@ -0,0 +1,103 @@
+<html> <body> <form method="get" action="target_proccessor.php"> <input type="search" name="search" /><br /> <input type="submit" name="submit" value="Search" /><br /> </form> <body> </html>
+
+
+
+$firstName = $_GET['firstName'] if(isset($firstName)){ echo "firstName field is set". "<br>"; } else{ echo "The field is not set."."<br>"; }
+$_POST["firstname"] or $_GET['lastname']
+
+<?php
+
+   $q = $_GET["q"];
+
+  
+
+   if($q == "cnn") {
+
+      $xml = ("http://rss.cnn.com/rss/cnn_topstories.rss");
+
+   }elseif($q == "bbc") {
+
+      $xml = ("http://newsrss.bbc.co.uk/rss/newsonline_world_edition/americas/rss.xml");
+
+   }elseif($q = "pcw"){
+
+      $xml = ("http://www.pcworld.com/index.rss");
+
+   }
+
+           
+
+   $xmlDoc = new DOMDocument();
+
+   $xmlDoc->load($xml);
+
+  
+
+   $channel = $xmlDoc->getElementsByTagName('channel')->item(0);
+
+  
+
+   $channel_title = $channel->getElementsByTagName('title')
+
+   ->item(0)->childNodes->item(0)->nodeValue;
+
+  
+
+   $channel_link = $channel->getElementsByTagName('link')
+
+   ->item(0)->childNodes->item(0)->nodeValue;
+
+  
+
+   $channel_desc = $channel->getElementsByTagName('description')
+
+   ->item(0)->childNodes->item(0)->nodeValue;
+
+  
+
+   echo("<p><a href = '" . $channel_link . "'>" .
+
+      $channel_title . "</a>");
+
+   echo("<br>");
+
+   echo($channel_desc . "</p>");
+
+  
+
+   $x = $xmlDoc->getElementsByTagName('item');
+
+  
+
+   for ($i = 0; $i<=2; $i++) {
+
+      $item_title = $x->item($i)->getElementsByTagName('title')
+
+      ->item(0)->childNodes->item(0)->nodeValue;
+
+     
+
+      $item_link = $x->item($i)->getElementsByTagName('link')
+
+      ->item(0)->childNodes->item(0)->nodeValue;
+
+     
+
+      $item_desc = $x->item($i)->getElementsByTagName('description')
+
+      ->item(0)->childNodes->item(0)->nodeValue;
+
+     
+
+      echo ("<p><a href = '" . $item_link . "'>" .
+
+         $item_title . "</a>");
+
+      echo ("<br>");
+
+      echo ($item_desc . "</p>");
+
+   }
+
+?>
+
